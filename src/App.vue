31b015fa4e77ba662c223d5a210142789a48d4dd
Fernanda <sanjaya.dev20@gmail.com>
2024-10-16 01:21:25 +0700

    add form add contact and dalate contact

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,60 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const formInput = ref({
+  name: '',
+  phone: ''
+})
+
+const dataContact = ref([
+  {
+    id: 1,
+    name: 'abdul',
+    phone: '123456789'
+  },
+  {
+    id: 2,
+    name: 'ahmad',
+    phone: '12345'
+  }
+])
+
+function addContact() {
+  dataContact.value.push({
+    id: dataContact.value.length + 1,
+    name: formInput.value.name,
+    phone: formInput.value.phone
+  })
+}
+
+function deleteContact(id : number) {
+  dataContact.value = dataContact.value.filter((contact) => contact.id !== id)
+}
+
+function editContact(id : number) {
+  const contact = dataContact.value.find((contact) => contact.id === id)
+  if (contact) {
+    formInput.value.name = contact.name
+    formInput.value.phone = contact.phone
+  }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello World</h1>
+  <form @submit.prevent="addContact">
+    <input type="text" placeholder="name" v-model="formInput.name" />
+    <input type="text" placeholder="phone" v-model="formInput.phone" />
+    <button>Add Contact</button>
+  </form>
+  <ul>
+    <li v-for="contact in dataContact" :key="contact.id">
+      <p>{{ contact.name }}</p>
+      <p>{{ contact.phone }}</p>
+      <button @click="editContact(contact.id)">Edit</button>
+      <button @click="deleteContact(contact.id)">Delete</button>
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
